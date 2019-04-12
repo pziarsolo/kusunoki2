d0dcafdf6bf0d789fd4b379ad9d711942f4384a1
peio <pziarsolo@upv.es>
2019-04-12 09:41:55 +0200

    add coulumn size to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'https://vavilov.comav.upv.es/api/',
+    centralColumnSize: '1200px',
     config: {
         defaultDataSource: {'code': 'CRF', kind: 'project'},
         defaultAccessionSetInstitute: 'ESP004'
