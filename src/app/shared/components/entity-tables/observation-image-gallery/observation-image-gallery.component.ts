d8ce32390bb3eb1c93b5c5e71f53e2b3aa2b34a3
peio <pziarsolo@upv.es>
2019-04-12 13:02:08 +0200

    add uid to image gallery object

--- src/app/shared/components/entity-tables/observation-image-gallery/observation-image-gallery.component.ts
+++ src/app/shared/components/entity-tables/observation-image-gallery/observation-image-gallery.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NgxGalleryOptions, NgxGalleryImage } from 'ngx-gallery';
+import { NgxGalleryOptions, NgxGalleryImage, INgxGalleryImage } from 'ngx-gallery';
 import { ObservationImageService } from 'src/app/shared/services/observation_image.service';
+import { DeleteDialogComponent } from '../../delete-dialog/delete-dialog.component';
+import { StatusService } from 'src/app/shared/StatusModule/status.service';
+import { MatDialog } from '@angular/material';
+import { SafeResourceUrl } from '@angular/platform-browser';
+
+export declare class NgxGalleryImageOwn implements INgxGalleryImage {
+    small?: string | SafeResourceUrl;
+    medium?: string | SafeResourceUrl;
+    big?: string | SafeResourceUrl;
+    description?: string;
+    url?: string;
+    label?: string;
+    uid?: string;
+    constructor(obj: INgxGalleryImage);
+}
 
 @Component({
   selector: 'kusunoki2-observation-image-gallery',
@@ -17,9 +32,11 @@ export class ObservationImageGalleryComponent implements OnInit {
         return this._searchParams;
     }
     galleryOptions: NgxGalleryOptions[];
-    galleryImages: NgxGalleryImage[];
+    galleryImages: NgxGalleryImageOwn[];
 
-    constructor(private service: ObservationImageService) { }
+    constructor(private service: ObservationImageService,
+        private readonly statusService: StatusService,
+        public dialog: MatDialog) { }
 
 
     doSearch(searchParams) {
@@ -27,7 +44,10 @@ export class ObservationImageGalleryComponent implements OnInit {
         this.service.list(searchParams)
             .subscribe(response => {
                 this.galleryImages = response.body.map(item => {
-                    return {small: item.image_small, medium: item.image_medium, big: item.image};
+                    return {small: item.image_small,
+                            medium: item.image_medium,
+                            big: item.image,
+                            uid: item.observation_image_uid};
                 });
             });
     }
@@ -42,7 +62,10 @@ export class ObservationImageGalleryComponent implements OnInit {
                 imageArrowsAutoHide: true, arrowPrevIcon: 'fa fa-angle-left',
                 arrowNextIcon: 'fa fa-angle-right', closeIcon: 'fa fa-times',
                 fullscreenIcon: 'fa fa-expands-arrows',
-                spinnerIcon: 'fa fa-refresh fa-spin fa-3x fa-fw', previewFullscreen: true
+                spinnerIcon: 'fa fa-refresh fa-spin fa-3x fa-fw', previewFullscreen: true,
+                actions: [{icon: 'fa fa-times-circle',
+                           onClick: this.delete.bind(this),
+                           titleText: 'delete'}]
             },
             {
                 breakpoint: 800, width: '100%', height: '400px',
@@ -54,6 +77,35 @@ export class ObservationImageGalleryComponent implements OnInit {
             {
                 breakpoint: 300, width: '100%', height: '200px', thumbnailsColumns: 2 }
         ];
+    }
+    delete(event, index) {
+        const image = this.galleryImages[index];
+        const dialogRef = this.dialog.open(DeleteDialogComponent, {
+            width: '800px',
+            data: {type: 'Image',
+                   description: `Image: ${image.uid}`}
+        });
+        dialogRef.afterClosed().subscribe(doDeleteInstitute => {
+            if (doDeleteInstitute) {
+                this.service.delete(image.uid)
+                    .subscribe(
+                        response => {
+                            this.statusService.info('Image sucessfully deleted');
+                            this.galleryImages.splice(index, 1);
+                            // this.instituteDeleted.emit();
+                        },
+                        error => {
+                            let msg;
+                            if (error.status === 401 || error.status === 403) {
+                                msg = 'Not authorized to delete the image';
+                            } else {
+                                msg = 'Could not delete Image';
+                            }
+                            this.statusService.error(msg);
+                        }
+                    );
+            }
+        });
     }
 }
 
