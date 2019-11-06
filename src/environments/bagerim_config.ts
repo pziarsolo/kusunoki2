1c1bb63c31107fd122ceb3895bda7574a19296db
peio <pziarsolo@upv.es>
2019-11-06 16:39:47 +0100

    update bagerim conf and update external modules

--- src/environments/bagerim_config.ts
+++ src/environments/bagerim_config.ts
@@ -1,8 +1,8 @@
 
 export const siteConfig = {
     pathToStaticPages: '/bagerim',
-    defaultDataSource: { 'code': 'ESP026', kind: 'genebank' },
-    defaultAccessionSetInstitute: 'ESP026',
+    defaultDataSource: { 'code': 'ESP133', kind: 'Governamental' },
+    defaultAccessionSetInstitute: 'ESP133',
     useAccessionset: false,
     centralColumnSize: '1200px',
     version: require('../../package.json').version,
