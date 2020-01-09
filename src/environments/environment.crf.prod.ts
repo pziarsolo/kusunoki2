210767f641992393acc088ea5c067f9bec56336e
peio <pziarsolo@upv.es>
2020-01-09 15:10:19 +0100

    update angular version and inia url

--- src/environments/environment.crf.prod.ts
+++ src/environments/environment.crf.prod.ts
@@ -2,6 +2,6 @@ import siteConfig from './crf_config';
 
 export const environment = {
     production: true,
-    apiUrl: 'https://tomatocrf.comav.upv.es/api/',
+    apiUrl: 'https://bancocrf.inia.es/api/',
     config: siteConfig
 };
