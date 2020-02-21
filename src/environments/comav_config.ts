638cbfc9ffa2b9e5fe7cb79f82325a6b937e541a
peio <pziarsolo@upv.es>
2020-02-21 14:50:21 +0100

    added comavs build conf for multilanguage

--- src/environments/comav_config.ts
+++ src/environments/comav_config.ts
@@ -3,12 +3,12 @@ export const siteConfig = {
     pathToStaticPages: '/comav_genebank',
     defaultDataSource: {'code': 'ESP026', kind: 'genebank'},
     defaultAccessionSetInstitute: 'ESP026',
-    languages: ['en'],
+    languages: ['en', 'es'],
     useAccessionset: false,
     centralColumnSize: '1200px',
     version: require('../../package.json').version,
-    useGoogleMaps: true,
-    googleMapsApiKey: 'AIzaSyBlw76orChFiR4RnGbMoNHMuF7t3g8MUgU'
+    useGoogleMaps: false,
+    googleMapsApiKey: 'undefined'
 };
 
 export default siteConfig;
---
+++ src/environments/environment.comav.es.prod.ts
@@ -0,0 +1,8 @@
+import siteConfig from './comav_config';
+
+export const environment = {
+    language: 'es',
+    production: true,
+    apiUrl: '/api/',
+    config: siteConfig
+};
