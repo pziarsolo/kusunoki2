1e91691a9e1f6b6d505a747b702cc23b93cede9b
peio <pziarsolo@upv.es>
2020-04-07 16:58:41 +0200

    more translations

--- src/app/pages/accession/assets/conservationStatus.ts
+++ src/app/pages/accession/assets/conservationStatus.ts
@@ -2,7 +2,7 @@ export const conservation_statuses = [
     {'code': 'is_active', 'name': 'Is part of active collection'},
     {'code': 'is_base', 'name': 'Is part of base collection'},
     {'code': 'is_active_and_base', 'name': 'Is part of active and base collection' },
-    { 'code': 'is_secundary', 'name': 'Is part of a secundary collection' },
+    {'code': 'is_secondary', 'name': 'Is part of a secondary collection' },
     {'code': 'is_archive', 'name': 'we have germplasm but not enougth'},
     {'code': 'is_historic', 'name': 'we only have documentation'}
 ];
