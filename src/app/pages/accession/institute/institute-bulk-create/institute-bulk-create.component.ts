7b33210db7cd85268ef72c4448f89146c6d675f7
peio <pziarsolo@gmail.com>
2020-07-07 11:59:52 +0200

    update institute example file and doc

--- src/app/pages/accession/institute/institute-bulk-create/institute-bulk-create.component.ts
+++ src/app/pages/accession/institute/institute-bulk-create/institute-bulk-create.component.ts
@@ -15,13 +15,14 @@ export class InstituteBulkCreateComponent extends BulkCreateComponent {
         {name: 'FULL_NAME', description: ['Name of the institute'], mandatory: true},
         {name: 'TYPE', description: ['Type of the institute: Can be Gobernmental, Genebank, Project...'],
             mandatory: false},
-        {name: 'STREET_POBE', description: ['Address of the institute'], mandatory: false},
+        {name: 'STREET_POB', description: ['Address of the institute'], mandatory: false},
         {name: 'CITY_STATE', description: ['City of the institute'], mandatory: false},
         {name: 'ZIP_CODE', description: ['Zip code of the institute'], mandatory: false},
         {name: 'PHONE', description: ['Phone number of the institute'], mandatory: false},
         {name: 'EMAIL', description: ['Email of contact of the institute'], mandatory: false},
         {name: 'URL', description: ['Url of the institute\' web page'], mandatory: false},
         {name: 'MANAGER', description: ['The database manager'], mandatory: false},
+        {name: 'COLLECTIONS', description: ['The database collections'], mandatory: false }
     ];
 }
 
